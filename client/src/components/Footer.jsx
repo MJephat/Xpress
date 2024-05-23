526b077bf1db1dde9474d48a27b7205ba8f2b133
MJephat <mukundijephat22@gmail.com>
2024-05-23 15:15:21 +0300

    client side done

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Logo from "../image/logo1.png";
+
+const Footer = () => {
+  return (
+    <footer>
+      <img src={Logo} alt="" />
+      <span>
+        made with passion and <b>React.js</b>
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
